15898507a71908c19773ac0be62f2b92df5d856c
Nikola Gjorgjievski <nikolaaaa1234@outlook.com>
2020-05-14 19:42:14 +0200

    Final commit for lab 3

--- src/lab3/MainAppLab3.java
+++ src/lab3/MainAppLab3.java
@@ -9,6 +9,12 @@ public class MainAppLab3 {
 		//ts.generateGraphic__X_R__Y_ALE__Change_err__Fixed_fa("non-iterative");
 		//ts.generateGraphic__X_err__Y_ALE__Change_R__Fixed_fa("non-iterative");
 		//ts.generateGraphic__X_fa__Y_ALE__Change_R__Fixed_err("non-iterative");
+		//ts.test_generateGraphic__X_R__Y_Loc__Change_fa__Fixed_err_Najbliski();
+		//ts.test_generateGraphic__X_R__Y_Loc__Change_fa__Fixed_err_Najrelevantni();
+		//ts.generateGraphic__X_R__Y_ALE__Change_err__Fixed_fa("iterative");
+		//ts.generateGraphic__X_err__Y_ALE__Change_R__Fixed_fa("iterative");
+		ts.generateGraphic__X_fa__Y_ALE__Change_R__Fixed_err("iterative");
+		
 	}
 
 }
